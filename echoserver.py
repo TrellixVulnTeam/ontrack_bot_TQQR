714eb0375b213642dddd728ec55e8c77a4f33109
simonkrol <simonkrol11@gmail.com>
2017-03-07 16:55:13 -0500

    redefine

--- echoserver.py
+++ echoserver.py
@@ -25,6 +25,7 @@ def handle_messages():
   print(payload)
   for sender, message in messaging_events(payload):
     print("Incoming from %s: %s" % (sender, message))
+    url=("https://graph.facebook.com/%i?&access_token=%i",sender,PAT)
     send_message(PAT, sender, message)
   return "ok"
 
