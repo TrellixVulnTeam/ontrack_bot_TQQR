b9b50cf9b105f755f1f0ac500f9c661551a7adc8
simonkrol <simonkrol11@gmail.com>
2017-03-15 00:26:06 -0400

    implement word fuzzing

--- chatbot/Scripts/activate.bat
+++ chatbot/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\konan\Desktop\Development\echoserver\echobot"
+set "VIRTUAL_ENV=C:\Users\konan\Desktop\Development\Python\on_chat\chatbot"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
@@ -9,7 +9,7 @@ if defined _OLD_VIRTUAL_PROMPT (
     )
     set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
 )
-set "PROMPT=(echobot) %PROMPT%"
+set "PROMPT=(chatbot) %PROMPT%"
 
 REM Don't use () to avoid problems with them in %PATH%
 if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
+from fuzzywuzzy import process
 import json
 import requests
 import urllib
 import config
 
-app = Flask(__name__)
 
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
+app = Flask(__name__)
+  
+
 try:
   PAT=os.environ['PAT']
   token=os.environ['token']
@@ -27,6 +28,7 @@ def handle_verification():
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
+
 def handle_messages():
   print("Handling Messages")
   payload = request.get_data()
@@ -34,40 +36,62 @@ def handle_messages():
 
   for sender, message in messaging_events(payload):
     print("Incoming from %s: %s" % (sender, message))
-    name=get_name(sender)
-    response=("Hey there %s!" %name)
-    send_message(PAT, sender, response)
+    message=message.decode("utf-8") 
+    #name=get_name(sender)
+    #response=("Hey there %s!" %name)
+    find_message(PAT, sender, message)
   return "ok"
+def find_message(PAT, sender, message):
+  lists=[]
+  lists.append({'greeting':['Hey', 'Hi', 'Hello', 'Hey there', "What's up?", 'watsup', 'Hiya', 'Aloha']})
+  lists.append({'farewell':['Bye', 'Seeya', 'See you later', 'Peace out', 'Goodbye']})
+  lists.append({'confirmation':['Yes', 'Yea', 'Please', 'Sure', 'Definitely', 'Of course', 'Mhmmm', 'Yes please']})
+  lists.append({'denial':['No', 'Nope', 'Maybe later', 'Maybe', 'No thanks']})
+  total=[]
+  for dicts in lists:
+    total+=(list(dicts.values())[0])
 
+  answer=process.extract(message, total)
+  for dicts in lists:
+    for title, words in dicts.items():  
+      if(answer[0][0] in words):
+        command=("%s(PAT, sender)" %title)
+        eval(command)
+
+def greeting(PAT, sender):
+  send_message(PAT, sender, "Hey %s, my name is Artemis, it's nice to meet you!" %get_name(sender))
+  send_message(PAT, sender, "Did you want to set up some tasks today?")
+def prompt():
+  return ("Did you want to set up some tasks today?")
+def farewell(PAT, sender):
+  send_message(PAT, sender, "Bye %s!" %get_name(sender))
+def confirmation(PAT, sender):
+  send_message(PAT, sender, "Awesome! Sadly, since I am currently in development, that feature is not fully functional and has yet to be released.")
+def denial(PAT, sender):
+  send_message(PAT, sender, "Okay, maybe later!")
+
+
+  
 
 def get_name(sender):
   url=("https://graph.facebook.com/%s" %sender)
   data=requests.get(url, params={'access_token': PAT})
   payload=json.loads(data.text)
   return payload['first_name']
+
 def messaging_events(payload):
-  """Generate tuples of (sender_id, message_text) from the
-  provided payload.
-  """
   data = json.loads(payload)
   messaging_events = data["entry"][0]["messaging"]
   for event in messaging_events:
     if "message" in event and "text" in event["message"]:
       yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-    #else:
-     #yield event["sender"]["id"], "I can't echo this"
-
 
 def send_message(token, recipient, text):
-  """Send the message text to recipient with id recipient.
-  """
-
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
     data=json.dumps({
       "recipient": {"id": recipient},
       "message": {"text": text}
-      #"message": {"text": text.decode('unicode_escape')}
     }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
